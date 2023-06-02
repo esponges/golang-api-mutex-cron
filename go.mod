4958b9da86c68c92e49258b7b0d04af36e352904
esponges <esponges@gmail.com>
2023-06-02 08:17:35 -0600

    remove extra files and implement cron in main

--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module golang-api-mutex-cron
 go 1.20
 
 require github.com/gorilla/mux v1.8.0
+
+require github.com/robfig/cron/v3 v3.0.0 // indirect
--- main.go
+++ main.go
@@ -3,10 +3,14 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"sync"
 	"time"
 
 	"github.com/gorilla/mux"
+	"github.com/robfig/cron/v3"
 )
+
+// TheStruct for the routing handler
 
 func main() {
 	// Create an instance of the struct
@@ -25,9 +29,47 @@ func main() {
 	router.HandleFunc("/", myInstance.Handler).Methods("GET")
 	router.HandleFunc("/read", myInstance.ReadHandler).Methods("GET")
 
+	// Create a new cron instance
+	c := cron.New()
+
+	// Add a new cron job that runs the IncrementGlobalVariable method every 10 seconds
+	c.AddFunc("*/10 * * * * *", func() {
+		myInstance.IncrementGlobalVariable()
+	})
+
+	// Start the cron instance
+	c.Start()
+
 	// Start the server
 	fmt.Println("Starting server on port 8080")
 	http.ListenAndServe(":8080", nil)
+}
+
+type MyStruct struct {
+	globalVariable int
+	mutex          sync.Mutex
+}
+
+func NewInstance() *MyStruct {
+	return &MyStruct{}
+}
+
+func (m *MyStruct) SetGlobalVariable(value int) {
+	m.mutex.Lock()
+	defer m.mutex.Unlock()
+	m.globalVariable = value
+}
+
+func (m *MyStruct) GetGlobalVariable() int {
+	m.mutex.Lock()
+	defer m.mutex.Unlock()
+	return m.globalVariable
+}
+
+func (m *MyStruct) IncrementGlobalVariable() {
+	m.mutex.Lock()
+	defer m.mutex.Unlock()
+	m.globalVariable++
 }
 
 func incrementGlobalVariable(m *MyStruct) {
@@ -41,4 +83,23 @@ func incrementGlobalVariable(m *MyStruct) {
 			m.IncrementGlobalVariable()
 		}
 	}
+}
+
+func (myInstance *MyStruct) Handler(w http.ResponseWriter, r *http.Request) {
+	// Update the global variable within the handler
+	myInstance.IncrementGlobalVariable()
+
+	// Access the global variable
+	value := myInstance.GetGlobalVariable()
+
+	// Respond to the request
+	fmt.Fprintf(w, "Global variable value: %d", value)
+}
+
+func (myInstance *MyStruct) ReadHandler(w http.ResponseWriter, r *http.Request) {
+	// Read the global variable
+	value := myInstance.GetGlobalVariable()
+
+	// Respond to the request
+	fmt.Fprintf(w, "Read global variable value: %d", value)
 }
